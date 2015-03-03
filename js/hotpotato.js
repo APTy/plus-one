9d808b2f2875beca1baa961b55b0851fd32ecb05
R. Tyler Julian <trjulian@gmail.com>
2015-03-03 14:23:02 -0800

    Add functionality to hotpotato.js

--- js/hotpotato.js
+++ js/hotpotato.js
@@ -2,11 +2,27 @@
 
 // run code after the document loads
 $(function() {
-  // get the current value
-  var num = $('#main').text();
+  // get and parse the current value
+  var num = +$('#main').text();
 
-  // parse and increment the current value
-  num = +num + 1;
+  // increment the current value
+  var numbers = [];
+  var numberCheck = true;
+  var prevNumber = 0;
+
+  // create an array of all numbers 1-100
+  for (var i = 1; i <= 100; i++) {
+    numbers.push(i);
+  }
+
+  // check to make sure every number is greater than the previous number
+  numbers.forEach(function(number) {
+    numberCheck = (number > prevNumber);
+    prevNumber = number;
+  });
+
+  // increment the index value if all goes well
+  num += +numberCheck;
 
   // print the current value to the DOM
   $('#main').text(num);
